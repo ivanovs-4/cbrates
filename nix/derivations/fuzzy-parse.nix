7c1270bb76757fdc69087170fec7751992e28bb1
Sergey Ivanov <ivanovs-4@yandex.ru>
2020-04-09 18:54:55 +0500

    fuzzy-parse via https address

--- nix/derivations/fuzzy-parse.nix
+++ nix/derivations/fuzzy-parse.nix
@@ -6,7 +6,7 @@ mkDerivation {
   pname = "fuzzy-parse";
   version = "0.1.1.0";
   src = fetchgit {
-    url = "git@github.com:hexresearch/fuzzy-parse.git";
+    url = "https://github.com/hexresearch/fuzzy-parse.git";
     sha256 = "0ss5npmxbv498ziblr766mz2q6cy5nkm6zi3y9cpfyb3bkax8cfj";
     rev = "99643c1d49822b943707c7f26b6777f2b227672e";
     fetchSubmodules = true;
